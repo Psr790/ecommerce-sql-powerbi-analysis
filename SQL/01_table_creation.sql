59d3bfd1a3d5f8b9a37a99f27d98f9bed45cf5c6
Psr790 <119902915+Psr790@users.noreply.github.com>
2026-04-30 19:13:37 +0200

    Update 01_table_creation.sql

--- SQL/01_table_creation.sql
+++ SQL/01_table_creation.sql
@@ -1,1 +1,79 @@
+/**********************************************************************************************
+Name: Pushpendra Singh Rathore
+Date: 30.04.2026
+Project: E-commerce Database Schema
+ Description: Core table creation and relationships for analysis
+**********************************************************************************************/
 
+-- Customers Table
+CREATE TABLE customers (
+    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_unique_id VARCHAR(50),
+    customer_zip_code_prefix VARCHAR(10),
+    customer_city VARCHAR(100),
+    customer_state VARCHAR(10)
+);
+
+
+-- Orders Table
+CREATE TABLE orders (
+    order_id VARCHAR(50) PRIMARY KEY,
+    customer_id VARCHAR(50),
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME
+);
+
+
+-- Products Table
+CREATE TABLE products (
+    product_id VARCHAR(50) PRIMARY KEY,
+    product_category_name VARCHAR(100)
+);
+
+
+-- Order Items Table
+CREATE TABLE order_items (
+    order_id VARCHAR(50),
+    order_item_id INT,
+    product_id VARCHAR(50),
+    seller_id VARCHAR(50),
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2)
+);
+
+
+-- Order Payments Table
+CREATE TABLE order_payments (
+    order_id VARCHAR(50),
+    payment_type VARCHAR(20),
+    payment_value DECIMAL(10,2)
+);
+
+
+-- Relationships
+
+-- Orders → Customers
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id);
+
+-- Order Items → Orders
+ALTER TABLE order_items
+ADD CONSTRAINT fk_items_orders
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
+
+-- Order Items → Products
+ALTER TABLE order_items
+ADD CONSTRAINT fk_items_products
+FOREIGN KEY (product_id)
+REFERENCES products(product_id);
+
+-- Payments → Orders
+ALTER TABLE order_payments
+ADD CONSTRAINT fk_payments_orders
+FOREIGN KEY (order_id)
+REFERENCES orders(order_id);
